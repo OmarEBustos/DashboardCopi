95f4e60652d5b9c70421d6982f7fadad4eeedbb3
OmarEBustos <omarbustos2013@gmail.com>
2024-02-04 08:13:14 -0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Notifications from 'vue-notification'
+import VuePaginate from 'vue-paginate'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { ModalPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$url = 'https://dashboardback-nmva.onrender.com/api';
+Vue.prototype.$token = localStorage.getItem('token');
+//Vue.prototype.$categorias = ['Ropa','Tecnologia','Juguetes','Hogar'];
+Vue.prototype.$ganancia = 0;
+
+
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "vue-search-select/dist/VueSearchSelect.css"
+
+Vue.use(Notifications)
+Vue.use(VuePaginate)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(ModalPlugin)
 
 new Vue({
   router,
